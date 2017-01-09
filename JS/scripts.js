3d4bf82f4240a41a9b78fc17acbef49acda1fe8c
mp and sp <sp@>
2017-01-09 15:48:19 -0800

    JS written

---
+++ JS/scripts.js
@@ -0,0 +1,7 @@
+var input = parseFloat(Prompt("What is the number of gallons you like to convert?"));
+var liter = input*3.78;
+var quart = input*4;
+var pint = input*8;
+var tablespn=input*256;
+
+alert("Your conversion result is " + liter + " liters and " + quart + " quarts and " + pint + " pints and " + tablespn + " table spoons.")
